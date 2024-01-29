7b609f3c5e6c7bfada25b7c7b68cec6b3b2759fd
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-01-29 18:23:04 +0200

    Styling

---
+++ game.js
@@ -0,0 +1,6 @@
+let gameName = "Guess Word Game";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} Developed By Mostafa Ahmed`;
